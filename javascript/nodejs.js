a34c589a0c1ebd6bcf304c9a1b89759f406ada3a
jeonjaekyun <jkyun01@gmail.com>
2020-05-04 16:39:14 +0900

    05.04.2020 commit

---
+++ javascript/nodejs.js
@@ -0,0 +1,9 @@
+var http = require('http');
+
+http.createServer(function(req, res){
+  res.writeHead(200, {'Context-Type':'text/plain'});
+  res.write('Hello World\n');
+  res.end();
+}).listen(1337, '127.0.0.1');
+
+console.log('Server running at http://127.0.0.1:1337/');
